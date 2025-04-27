278b144357558166640fddfe4eac489eac101b70
Flávio Irala <flavioiralap@gmail.com>
2025-04-27 15:10:02 -0400

    Navegação entre rotas concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import EstiloGlobal, { Container } from './styles'
-import BarraLateral from './constainers/BarraLateral'
-import ListaDeTarefas from './constainers/listaDeTarefas'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import { Provider } from 'react-redux'
 import store from './store'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import BotaoAdicionar from '../../components/BotaoAdicionar'
+import BarraLateral from '../../constainers/BarraLateral'
+import ListaDeTarefas from '../../constainers/listaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+    <BotaoAdicionar />
+  </>
+)
+
+export default Home
